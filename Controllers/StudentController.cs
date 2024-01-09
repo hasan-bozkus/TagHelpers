d8b7bb3d92bec930ece94761929caa68dbf5b056
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-01-09 21:32:44 +0300

    Tag Helper 2

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TagHelpers.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [Route("student/{id:int}")]
+        public IActionResult Detail(int id) => View();
+
+        [Route("student/list", Name = "getlistroute")]
+        public IActionResult GetList() => View();
+
+        [Route("/student/filter", Name = "filterstudentroute")]
+        public IActionResult GetList(int age, string name) => View();
+    }
+}
---
+++ Models/MetaTagHelperComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace TagHelpers.Models
+{
+    public class MetaTagHelperComponent : TagHelperComponent
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.Equals(context.TagName, "head", StringComparison.OrdinalIgnoreCase))
+            {
+                output.PostContent.AppendHtml($"<meta name=\"title\" content=\"Sayfanın Başlığı\" /> \r\n");
+                output.PostContent.AppendHtml($"<meta name=\"description\" content=\"Sayfanın İzahati\" \r\n />");
+            }
+        }
+    }
+}
